ad777758a9c566aae3aa8df27f06b5dbbdfdceca
Raphael Gruber <raphi011@gmail.com>
2024-06-18 12:58:18 +0200

    improve date formatting in pyramid dropdown

--- app/dropdown.tsx
+++ app/dropdown.tsx
@@ -15,10 +15,20 @@ export default function Dropdown({ standings, selected, onChange }) {
       >
         {standings.map((s, i) => (
           <option value={i} key={s.date}>
-            {new Date(s.date).toLocaleString()}
+            {formatDate(new Date(s.date))} {i == 0 ? "(Aktuell)" : ""}
           </option>
         ))}
       </select>
     </div>
   );
+}
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
